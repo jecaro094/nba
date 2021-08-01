48c8aa6e6e325e80fc98639ab44ae428e9583ce8
jecaro094 <jecarobd@gmail.com>
2021-08-01 14:28:18 +0200

    Cleaned code from python

--- app.py
+++ app.py
@@ -3,22 +3,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-import random # remove
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 # functions
 
 def get_sort(sort):
+    """
+    Consider ascending/descending
+    """
     if sort=="Ascending":
         return True
     elif sort=="Descending":
         return False
 
+def get_bool_from_drop(drop):
+    """
+    Consider (or not) zero values in DF
+    """
+    if drop=="Exclude zeros":
+        return True
+    elif drop=="Do not exclude zeros":
+        return False
+
 def get_df_stat(text_stat):
     """
-    Returns the column name used in the dataframe,
-    considering the text for the stat
+    Returns the stat name for the DF
     """
     stat = text_stat.split()[0]
     if stat!="Minutes":
@@ -26,55 +35,87 @@ def get_df_stat(text_stat):
     else:
         return "MPG"
     
-async def generate_graph_image(stat, sort, limit):
+async def generate_graph_image(stat, sort, limit, drop_zeros):
+    """
+    Generate graph from the DF and parameters
+    """
     stat_selected = get_df_stat(stat) 
-    df_sorted = df_nba.sort_values(stat_selected, ascending=get_sort(sort))[:int(limit)]
-    ax = df_sorted.plot.bar(x='FULL NAME', y=stat_selected, rot=90)
-    # not to show graph
+    df_nba_ = df_nba.copy()
+    
+    # NOTE Exclude zero values from a copy of the original DF, for the stat considered
+    for index, row in df_nba_.iterrows():
+        if not (row[stat_selected]) and get_bool_from_drop(drop_zeros):
+            df_nba_ = df_nba_.drop([index])
 
-    # Saved in the same folder
-    plt.savefig(figure_path, bbox_inches='tight')
+    # NOTE Get the figure, considering the parameters of this function
+    df_sorted = df_nba_.sort_values(stat_selected, ascending=get_sort(sort))[:int(limit)]
+    ax = df_sorted.plot.bar(x='FULL NAME', y=stat_selected, rot=90)
+
+    # NOTE Saved graph figure in "figure_path" folder
+    plt.savefig(figure_path, bbox_inches='tight', dpi=200)
+
+    # NOTE Close open figure
+    plt.close('all')
+
     
 def get_param(url, param):
+    """
+    Returns the value of the specified parameter
+    from a URL string
+    """
     import urllib.parse as urlparse
     from urllib.parse import parse_qs
     parsed = urlparse.urlparse(url)
     return (parse_qs(parsed.query)[param])[0]
 
-def get_options():
-    return [stats, lim, arrange]
+def get_params(url):
+    """
+    Returns all parameters from URL at once
+    """
+  
+    return [get_param(url, "statics"), get_param(url, "limit"), \
+            get_param(url, "arrange"), get_param(url, "drop")]
+
+def get_options(default_options=False):
+    """
+    Returns options to be displayed in the HTML
+    """
+    if default_options:
+        return [stats[0], lim[0], arrange[0], drop[0]]
+    else:
+        return [stats, lim, arrange, drop]
+
+# NOTE Define a DF and options to be displayed in HTML page
 
 csv_path = os.path.join(os.getcwd(), "NBA2021.csv")
 figure_path = os.path.join(os.getcwd(),"..", "static", 'saved_figure.png')
 with open(csv_path, "r") as csv_file:
     df_nba = pd.read_csv(csv_file)
 
+drop = ["Exclude zeros", "Do not exlude zeros"]
 stats = ["Points per Game", "Assists per Game", "Rebounds per Game", \
         "Steals per Game", "Minutes per Game"]
 lim = ["5","10","15","20","25"]
 arrange= ["Ascending", "Descending"]
 
+# NOTE Defining the app
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 async def hello_world():
+    return redirect("/nbastats")
 
-    url = request.url
-    [stat, lim, arr] = get_options()
-
-    await generate_graph_image(stat[0], arr[0], lim[0])
-    return render_template('page.html', options_=get_options(), arr=arr[0], lim=lim[0], stat=stat[0])
-
-@app.route('/nbastats', methods=['GET', 'POST'])
+@app.route('/nbastats', methods=['GET'])
 async def stats_function():
 
-    stat = get_param(request.url, "statics")
-    lim = get_param(request.url, "limit")
-    arr = get_param(request.url, "arrange")
+    try:
+        [stat, lim, arr, drp] = get_params(request.url)
+    except:
+        [stat, lim, arr, drp] = get_options(default_options=True)
 
-    await generate_graph_image(stat, arr, lim)
-    return render_template('page.html', options_=get_options(), arr = arr, lim=lim, stat=stat)
+    await generate_graph_image(stat, arr, lim, drp)
+    return render_template('page.html', options_=get_options(), arr = arr, lim=lim, stat=stat, drp=drp)
 
 if __name__ == '__main__':
     app.run()   
